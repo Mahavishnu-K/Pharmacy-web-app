79437e725202f0cb3c9e7ecf0460ef1ef501d4ba
Mahavishnu-K <mahavishnu005sk@gmail.com>
2025-03-17 20:21:53 +0530

    docker config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: Number(process.env.VITE_PORT) || 5173,
+    host: true
+  },
   plugins: [react()],
 })
